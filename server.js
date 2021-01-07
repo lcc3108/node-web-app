b2919419acb161fee6544862b6eddd701aaec741
IM CHAECHEOL <dlacocjf32@gmail.com>
2021-01-07 21:40:50 +0900

    webhook test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 
 // Constants
-const PORT = process.env.PORT || "8080";
+const PORT = process.env.PORT || "80";
 const HOST = process.env.HOST || "0.0.0.0";
 const VERSION = process.env.VERSION || "0.0";
 
